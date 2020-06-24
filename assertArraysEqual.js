188f24c346db0720ac8f52a7df1d8beb4fbd3529
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-06-23 22:59:23 -0700

    assertArrayEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+module.exports = assertArraysEqual;
