6deb6be2d5251047bffcd21e934325240de9cda5
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-06-28 20:45:55 -0700

    added template literal ${}

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,6 @@
 // FUNCTION IMPLEMENTATION 
+//template literals, template strings
+//which allow you to insert variables / JS into strings by using back-ticks `` and the ${} symbol.
 
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
@@ -11,4 +13,6 @@ console.log(assertEqual("Natalia", "Natalia"));
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(123, 123));
 console.log(assertEqual(3, 2));
+
+//this is used for other files, do not delete
 module.exports = assertEqual;
