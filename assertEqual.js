3ae1d3d386c6adc8142ca28f55f959b66b2fa21c
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-06-28 19:44:57 -0700

    cleaned up the code

--- assertEqual.js
+++ assertEqual.js
@@ -7,6 +7,8 @@ const assertEqual = function(actual, expected) {
         console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
     }
 };
+console.log(assertEqual("Natalia", "Natalia"));
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
+console.log(assertEqual(123, 123));
+console.log(assertEqual(3, 2));
 module.exports = assertEqual;
