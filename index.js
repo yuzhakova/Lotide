5a8fe65aca116ad13e91060651388535a110f37a
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-06-29 21:09:34 -0700

    all functions added

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+
+const assertArraysEqual  = require('./assertArraysEqual');
+const assertEqual        = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters       = require('./countLetters');
+const countOnly          = require('./countOnly');
+const eqArrays           = require('./eqArrays');
+const eqObjects          = require('./eqObjects');
+const findKey            = require('./findKey');
+const findKeyByValue     = require('./findKeyByValue');
+const head               = require('./head');
+const letterPositions    = require('./letterPositions');
+const map                = require('./map');
+const middle             = require('./middle');
+const tail               = require('./tail');
+const takeUntil          = require('./takeUntil');
+const without            = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without,
 };
