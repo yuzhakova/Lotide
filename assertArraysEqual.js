750fde278578351b9792e15115a5a0a1cb6c536f
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-06-29 17:13:42 -0700

    refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,20 @@
+// const eqArrays = require('./eqArrays');
+
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+//   }
+// };
+
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
+    cconsole.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+      }
+    };
 module.exports = assertArraysEqual;
